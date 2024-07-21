bb2dd1852eb3d2971a8e3c24b8e82a827d91ac65
Fabian Distler <fabian.distler@gmx.net>
2024-07-21 13:50:00 +0200

    clean.

--- R/setupR-package.R
+++ R/setupR-package.R
@@ -2,9 +2,5 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom lifecycle deprecated
-#' @importFrom usethis edit_r_profile
-#' @importFrom usethis use_template
-#' @importFrom utils writeClipboard
 ## usethis namespace: end
 NULL
